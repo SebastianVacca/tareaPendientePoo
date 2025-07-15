82c34d80b2a1766a20fb599f6f9ce7db9fa5047f
Juan Vacca <clanbs11.12@gmail.com>
2025-07-15 10:55:09 -0500

    Add method uses

--- src/App.java
+++ src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        var task = new Tarea("Hacer aseo", 1);
+
+        System.out.println(task.showState());
+        task.setMarkCompleted();
+
+        System.out.println(task.showState());
     }
 }
